0b11318db57a955fedc8d92edfcfd6d4641a5926
Matěj Kubíček <kubicekm23@spstrutnov.cz>
2025-03-31 12:05:38 +0200

    Příprava pro přidání uživatelů

--- SenkyrExplanation/WebApplication1/Controllers/UserController.cs
+++ SenkyrExplanation/WebApplication1/Controllers/UserController.cs
@@ -1,8 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication1.Controllers;
 
 public class UserController : Controller
 {
+    private readonly DbContext _context;
     
+    public UserController(DbContext context)
+    {
+        _context = context;
+    }
+    
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
 }
